1671689187facf357f8d059aa85605c4643e61c6
zhao.zijun <1306778108@qq.com>
2022-02-08 11:42:54 +0800

    install vue-router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
-import {Button} from 'element-ui'
+import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
-Vue.use(Button)
+import router from '../router'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
